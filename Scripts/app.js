18938f214aa8fcea816fb99b1eec397263179b9f
gdarley <gdarley@hotmail.com>
2014-12-07 16:18:19 -0200

    added content: accordion, lists, true info, maps code (not working)

--- Scripts/app.js
+++ Scripts/app.js
@@ -148,3 +148,20 @@
 
     */
 })();       //?
+     function initialize() {
+        var mapCanvas = document.getElementById('map-canvas');
+        var mapCanvas2 = document.getElementById('map2-canvas');
+        var mapOptions1 = {
+          center: new google.maps.LatLng(44.5403, -78.5463),
+          zoom: 8,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        var mapOptions2 = {
+          center: new google.maps.LatLng(44.5403, -78.5463),
+          zoom: 8,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        var map = new google.maps.Map(mapCanvas, mapOptions)
+        var map2 = new google.maps.Map(mapCanvas2, mapOptions2)
+      }
+      google.maps.event.addDomListener(window, 'load', initialize);
